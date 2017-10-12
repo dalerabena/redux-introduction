ce5c700eb425f4a1a0559c82c2c5d15642280b07
dsrabena <dsrabena@mmsu.edu.ph>
2017-10-12 16:48:45 +0800

    update error

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,15 @@
 import Rebase from 're-base';
 import firebase from 'firebase';
 
-var app = firebase.initializeApp({
-  
-});
+// var app = firebase.initializeApp({
+//   apiKey: "",
+//   authDomain: "",
+//   databaseURL: "",
+//   projectId: "",
+//   storageBucket: "",
+//   messagingSenderId: ""
+// });
 
-const base = Rebase.createClass(app.database());
+// const base = Rebase.createClass(app.database());
 
-export default base;
+// export default base;
